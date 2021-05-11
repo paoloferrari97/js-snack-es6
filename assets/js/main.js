d849736101e846d486eb20971c462bfa765aa26e
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-11 15:15:50 +0200

    Inizio snack 1

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+//Snack 1:
+//Creare un array di oggetti, ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+//nome e peso.
+//Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+let biciclette = [
+    {
+        nome: "Cannondale SuperSix Evo Hi-Mod Disc",
+        peso: 7.3
+    },
+    {
+        nome: "Giant TCR Advanced Pro 0 Disc",
+        peso: 6.8
+    },
+    {
+        nome: "Specialized S-Works Tarmac SL7 Dura-Ace Di2",
+        peso: 6.8
+    },
+    {
+        nome: "Pinarello Dogma F12",
+        peso: 5.84
+    },
+    {
+        nome: "Cannondale SystemSix Hi-Mod",
+        peso: 7.6
+    },
+    {
+        nome: "Trek Madone SLR 9 Disc eTap",
+        peso: 7.98
+    },
+    {
+        nome: "Canyon Aeroad CFR Di2",
+        peso: 7.3
+    },
+];
