0dcab37ba655c96865a4f47229944b17596b8f79
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-11 15:52:57 +0200

    Inizio Snack 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ let biciclette = [
     {
         nome: "Canyon Aeroad CFR Di2",
         peso: 7.3
-    },
+    }
 ];
 
 let indice;
@@ -52,3 +52,67 @@ document.getElementById("bici_peso_minore").insertAdjacentHTML("beforeend",
     La bicicletta con il peso minore è: ${nome}, e il suo peso è di ${peso} kg!
     `
 );
+
+
+
+//Snack2:
+//Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà:
+//nome, punti fatti, falli subiti.
+//Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+//Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+//Infine usando la destrutturazione creiamo un nuovo array i cui elementi 
+//contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+let squadre = [
+    {
+        nome: "Juventus",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Brescia",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Milan",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Inter",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Parma",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Palermo",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Roma",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Lazio",
+        puntiFatti: 0,
+        falliSubiti: 0
+    }    
+];
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+for (var i = 0; i < squadre.length; i++){
+    squadre[i].puntiFatti = getRandomIntInclusive(0, 100);
+    squadre[i].falliSubiti = getRandomIntInclusive(0, 3000);
+}
