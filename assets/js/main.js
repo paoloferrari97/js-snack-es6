de0417907ce118b07b9946d4e4baec0f69ef78b0
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-12 15:16:17 +0200

    Snack 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,3 +128,26 @@ for (var i = 0; i < squadre.length; i++){
 }
 
 console.log(nuovoArray);
+
+
+
+//Snack 3:
+//Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+//La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+//Usiamo i nuovi metodi degli array foreach o filter.
+
+const numeri = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const numeroUno = 5;
+const numeroDue = 16;
+
+//1 modo
+let nuoviNumeri = [];
+
+numeri.forEach((numero, i, numeri) => {
+    if (i > numeroUno && i < numeroDue) {
+        nuoviNumeri.push(numero);
+    }
+});
+
+console.log(numeri);
+console.log(nuoviNumeri);
